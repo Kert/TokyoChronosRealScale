de41569449451b0446e44cf838133f736c446656
Sergei Nikitin <kertwaii@gmail.com>
2025-11-02 19:14:50 +0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 dotnet build --configuration Release
-COPY ".\bin\Release\net3.5\TokyoChronosRealScale.dll" "C:\Steam\steamapps\common\TOKYO CHRONOS\BepInEx\plugins\TokyoChronosRealScale.dll" /Y
+COPY ".\bin\Release\net3.5\TokyoChronosRealScale.dll" "C:\Program Files (x86)\Steam\steamapps\common\TOKYO CHRONOS\BepInEx\plugins\TokyoChronosRealScale.dll" /Y
 pause
