8b4f48060522ba136099abe9cfcc982cf3b8f80a
Kert <kertwaii@gmail.com>
2025-11-05 19:30:29 +0600

    Fixed closeup scenes having incorrect scale

--- TokyoChronosRealScale.cs
+++ TokyoChronosRealScale.cs
@@ -9,6 +9,10 @@ namespace TokyoChronosRealScale
     public partial class TokyoChronosRealScale : BaseUnityPlugin
     {
         const float WORLD_SCALE = 3.0f; // Real life floor is synced perfectly with VR when used with this scale
+        const float CLOSEUP_SCENE_SCALE = WORLD_SCALE / 2;
+        Transform world = null;
+        // used to fix laser pointer size
+        Transform rightController = null;
         // used to get data from private fields with reflection
         const BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
 
@@ -19,7 +23,8 @@ namespace TokyoChronosRealScale
             {
                 // hooks
                 On.TkChronos.PlayAreaManager.InitMover += PlayAreaManager_InitMover;
-                On.TkChronos.PlayAreaManager.AttachTrackingObjects += PlayAreaManager_AttachTrackingObjects;                
+                On.TkChronos.PlayAreaManager.AttachTrackingObjects += PlayAreaManager_AttachTrackingObjects; 
+                On.TkChronos.PlayAreaManager.SetLayerMode += PlayAreaManager_SetLayerMode;
             }
             catch (Exception e)
             {
@@ -31,18 +36,32 @@ namespace TokyoChronosRealScale
 
         void PlayAreaManager_InitMover(On.TkChronos.PlayAreaManager.orig_InitMover orig, TkChronos.PlayAreaManager self)
         {
-            self.VRAreaTransform.localScale = new Vector3(WORLD_SCALE, WORLD_SCALE, WORLD_SCALE);
-            Logger.LogInfo("Real world scale set");
+            world = self.VRAreaTransform;
             orig(self);
         }
 
         void PlayAreaManager_AttachTrackingObjects(On.TkChronos.PlayAreaManager.orig_AttachTrackingObjects orig, TkChronos.PlayAreaManager self)
         {
             TkChronos.TrackingObjectComponents component = (TkChronos.TrackingObjectComponents)typeof(TkChronos.PlayAreaManager).GetField("userTrackComponent", bindingFlags).GetValue(self);
-            float s = 1.0f / WORLD_SCALE;
-            component.rightHandComponent.transform.localScale = new Vector3(s, s, s);
-            Logger.LogInfo("Laser pointer scale set");
+            rightController = component.rightHandComponent.transform;
             orig(self);
+        }
+
+        void PlayAreaManager_SetLayerMode(On.TkChronos.PlayAreaManager.orig_SetLayerMode orig, TkChronos.PlayAreaManager self, TkChronos.PlayAreaManager.LayerModes mode)
+        {
+            if(mode == TkChronos.PlayAreaManager.LayerModes.BokeSphere)
+                SetScale(CLOSEUP_SCENE_SCALE);
+            else
+                SetScale(WORLD_SCALE);
+            orig(self, mode);
+        }
+
+        void SetScale(float scale)
+        {
+            Logger.LogInfo("Game Scale set to " + scale.ToString());
+            world.localScale = new Vector3(scale, scale, scale);            
+            float s = 1.0f / scale;
+            rightController.localScale = new Vector3(s, s, s);
         }
     }
 }
